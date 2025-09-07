f8030b656b1316ab89386232c20feb0bafe770cb
yousefsalahalbibi54-oss <yousef.salahalbibi54@gmail.com>
2025-09-07 05:30:50 +0300

    Create package.js
    Signed-off-by: yousefsalahalbibi54-oss <yousef.salahalbibi54@gmail.com>

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "gta-rp-bot",
+  "version": "1.0.0",
+  "description": "Discord bot for GTA RP server (Node.js)",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "deploy": "node deploy-commands.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.15.3",
+    "express": "^4.18.2",
+    "dotenv": "^16.3.1"
+  }
+}
