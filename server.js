a13297465368287168a8d2148e7cdc0ad5d6a96a
yousefsalahalbibi54-oss <yousef.salahalbibi54@gmail.com>
2025-09-07 03:13:43 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("✅ Bot is running...");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🌐 Server running on port ${PORT}`);
+});
